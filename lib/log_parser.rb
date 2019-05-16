1505cf939a3dcec4d4d02180a5cbc3c1a27df03c
Mateusz Luterek <mateusz.luterek@lunarlogic.io>
2019-05-16 19:05:13 +0200

    Update LogParser to use StatsPresenter

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,10 +1,13 @@
+require_relative "stats_presenter"
+
 class LogParser
-  def self.parse(path)
-    new(path).call
+  def self.parse(path, stats_presenter = StatsPresenter)
+    new(path, stats_presenter).call
   end
 
-  def initialize(path)
+  def initialize(path, stats_presenter)
     self.path = path
+    self.stats_presenter = stats_presenter
   end
 
   def call
@@ -19,14 +22,13 @@ class LogParser
     page_views_counts = page_views.map { |path, ips| [path, ips.count] }
     unique_page_views_counts = page_views.map { |path, ips| [path, ips.uniq.count] }
 
-    puts page_views_counts
-    puts "\n"
-    puts unique_page_views_counts
+    stats_presenter.print_page_views(page_views_counts)
+    stats_presenter.print_unique_page_views(unique_page_views_counts)
   end
 
   private
 
-  attr_accessor :path
+  attr_accessor :path, :stats_presenter
 
   def parse_file(file)
     page_views = Hash.new { |hash, key| hash[key] = [] }
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -2,11 +2,22 @@ require "log_parser"
 
 describe LogParser do
   describe "::parse" do
-    subject { described_class.parse(path) }
+    subject { described_class.parse(path, stats_presenter) }
     let(:path) { "spec/support/test_data.log" }
+    let(:stats_presenter) { double("StatsPresenter", print_page_views: nil, print_unique_page_views: nil) }
 
     it "reads the file from the given path" do
       expect(File).to receive(:read).with(path).and_return("")
+
+      subject
+    end
+
+    it "uses the stats presenter to print the calculated values" do
+      expected_page_views = [["/3", 1], ["/2", 2], ["/1", 3]]
+      expected_unique_page_views = [["/3", 1], ["/2", 2], ["/1", 1]]
+
+      expect(stats_presenter).to receive(:print_page_views).with(expected_page_views)
+      expect(stats_presenter).to receive(:print_unique_page_views).with(expected_unique_page_views)
 
       subject
     end
